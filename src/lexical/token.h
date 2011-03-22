c8099dce788239424f585290bdfc8fb0ddc1d45f
Leandro Santiago <leandrosansilva@gmail.com>
2011-03-22 15:37:42 -0300

    incrementando estrutura para o futuro compiladorx

---
+++ src/lexical/token.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+
+namespace Lexical {
+
+/*
+ * FIXME: estou confundindo a definição de um token com sua instância :-(
+*/
+  
+class Token {
+public:
+  /* Quais as classes de token? */
+  enum type {
+    IDENTIFIER, // identificador
+    RESERVED, // palavra reservada
+    SYMBOL, // símbolo
+    CONSTANT, // constante
+    STRING, // constante de texto?
+    COMMENT // comentário: quem disse que comentários não são úteis? :-)
+  };
+
+private:
+  /* O token de um identificador */
+  type _type;
+  
+  /* O lexema, ou string achada */
+  std::string _lexema;
+  
+  /* Qual padrão que casa o token */
+  std::string _pattern; 
+};
+
+}
